cfb560a72dcb04e005d81d9e943ea187ddd132cd
taoprogramer <taoprogramer@gmail.com>
2016-02-18 19:04:48 +0800

    remove some test files in app:host

--- host/test2.py
+++
@@ -1,6 +0,0 @@
-import redis
-REDIS_URI = 'redis://sys-redis.rzwxgy.0001.cnn1.cache.amazonaws.com.cn:3807/3'
-ri = redis.Redis.from_url(REDIS_URI)
-ri = redis.StrictRedis(host=REDIS_URI)
-print ri.echo("dsdsds")
-print ri.get("gn86azx2jornynxupuffwsapf9y82gfw")
--- host/tests.py
+++ host/tests.py
@@ -1,18 +1,2 @@
 from django.test import TestCase
 # Create your tests here.
-from ansible.inventory import Inventory,Group
-from ansible.parsing.dataloader import DataLoader
-from ansible.vars import VariableManager
-import os
-from ConfigParser import SafeConfigParser,ConfigParser,RawConfigParser
-
-if __name__ == '__main__':
-    hosts_file = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'host', 'hosts')
-    loader = DataLoader()
-    variable_manager = VariableManager()
-    inventory = Inventory(loader,variable_manager,hosts_file)
-    print inventory.groups
-    print inventory.list_groups()
-    print inventory.add_group(Group('dwdwdw'))
-    print inventory.list_groups()
-    inventory.serialize()
